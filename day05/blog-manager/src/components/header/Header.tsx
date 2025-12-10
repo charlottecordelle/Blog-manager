a9f1b65fc35d9cee91f6ec0caa8a32e55cf99ab2
Charlotte Cordelle <cordel_c@etna-alternance.net>
2025-12-10 17:23:59 +0100

    derniere version du header

--- day05/blog-manager/src/components/header/Header.tsx
+++ day05/blog-manager/src/components/header/Header.tsx
@@ -1,26 +1,46 @@
-import { Link } from 'react-router-dom';
-import './Header.css';
+import { Link, useLocation } from "react-router-dom";
+import { Home, Menu, Plus } from "lucide-react";
+import "./Header.css";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className="header">
-      <nav className="nav-container">
-        <Link to="/" className="logo">
-          Mon Site
-        </Link>
-        
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Accueil</Link>
-          </li> 
-          <li>
-            <Link to="/all">Tout les articles</Link>
-          </li>
-          <li>
-            <Link to="/new">Nouvel Article</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="header-container">
+        <div className="logo-box">
+          <span>C</span>
+        </div>
+
+        <h1 className="header-title">CHA'BLOG</h1>
+
+        <div className="header-icons">
+          <Link
+            to="/"
+            className={`icon-button ${
+              location.pathname === "/" ? "active-button" : ""
+            }`}
+          >
+            <Home size={20} />
+          </Link>
+          <Link
+            to="/all"
+            className={`icon-button ${
+              location.pathname === "/all" ? "active-button" : ""
+            }`}
+          >
+            <Menu size={20} />
+          </Link>
+          <Link
+            to="/new"
+            className={`icon-button ${
+              location.pathname === "/new" ? "active-button" : ""
+            }`}
+          >
+            <Plus size={20} />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
